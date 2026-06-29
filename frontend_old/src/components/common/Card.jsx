05017b7303b54774e245cac8f0dc68d41440c158
Pazhanisha <mspazhanisha@gmail.com>
2026-06-29 12:41:58 +0530

    Clean project structure and remove old frontend

--- frontend_old/src/components/common/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className = "" }) {
-  return (
-    <div className={`rounded-[2rem] border border-white/10 bg-white/5 backdrop-blur-xl ${className}`}>
-      {children}
-    </div>
-  );
-}
